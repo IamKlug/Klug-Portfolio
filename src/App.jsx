90a44c5db78be4bd7f3c7d71f9203a3d18b0007b
Martin <Martinkn13@gmail.com>
2023-09-02 06:56:32 -0700

    another try at getting images to function with vercel

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function App() {
           <div className='img-width25 is-relative m-3 '>
 
           <figure className='image is-3by5'>  
-            <img loading='lazy' className='is-rounded' src='tiny-head-shot.png' />
-            
+            <img className='is-rounded' src='src/assets/tiny-head-shot.png' />
+
           </figure>
           </div>
             <p className='subtitle has-text-centered'>I build software</p>
@@ -36,7 +36,7 @@ function App() {
                 <div className='is-relative'>
                     <span className='tag is-link is-medium'>Click the Image to check it out!</span>
                     <figure className='image'>
-                      <a href='https://klug-task-management-git-master-iamklug.vercel.app/'><img loading='lazy' className='' src='/tiny-kanban-image.png' /></a>
+                      <a href='https://klug-task-management-git-master-iamklug.vercel.app/'><img className='' src='src/assets/tiny-kanban-image.png' /></a>
                     </figure>
                   </div>
                 </div>
@@ -46,7 +46,7 @@ function App() {
                 <div className='is-relative'>
                 <span className='tag is-link is-medium'>This one too! This one's a treat!</span>
                     <figure className='image'>
-                      <a href='https://multi-step-form-vercel-iamklug.vercel.app/'><img loading='lazy' className='' src='/tiny-form-image.png' /></a>
+                      <a href='https://multi-step-form-vercel-iamklug.vercel.app/'><img className='' src='src/assets/tiny-form-image.png' /></a>
                     </figure>
                   </div>
                 </div>
