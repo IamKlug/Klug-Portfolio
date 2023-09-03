a2c93370fac1f6979457cadb640193759b33c52e
Martin <Martinkn13@gmail.com>
2023-09-03 11:27:22 -0700

    images again

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <div className='img-width25 is-relative m-3 '>
 
           <figure className='image is-3by5'>  
-            <img className='is-rounded' src='/tiny-head-shot.png' />
+            <img className='is-rounded' src='src/assets/tiny-head-shot.png' />
 
           </figure>
           </div>
@@ -36,7 +36,7 @@ function App() {
                 <div className='is-relative'>
                     <span className='tag is-link is-medium'>Click the Image to check it out!</span>
                     <figure className='image'>
-                      <a href='https://klug-task-management-git-master-iamklug.vercel.app/'><img className='' src='/tiny-kanban-image.png' /></a>
+                      <a href='https://klug-task-management-git-master-iamklug.vercel.app/'><img src='src/assets/tiny-kanban-image.png' /></a>
                     </figure>
                   </div>
                 </div>
@@ -46,7 +46,7 @@ function App() {
                 <div className='is-relative'>
                 <span className='tag is-link is-medium'>This one too! This one's a treat!</span>
                     <figure className='image'>
-                      <a href='https://multi-step-form-vercel-iamklug.vercel.app/'><img className='' src='/tiny-form-image.png' /></a>
+                      <a href='https://multi-step-form-vercel-iamklug.vercel.app/'><img src='src/assets/tiny-form-image.png' /></a>
                     </figure>
                   </div>
                 </div>
@@ -78,7 +78,7 @@ function App() {
           </div>
         </div>
 
-        <div className='is-size- has-text-centered is-underlined'>
+        <div className='has-text-centered is-underlined'>
           Skills in development 
         </div>
 
